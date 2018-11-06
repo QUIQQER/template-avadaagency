00fb32327bc321566bb6797fa1028bf8794daac5
michael.daniel <michael@pcsg.de>
2018-11-06 16:41:33 +0100

    feat: Subtemplates functionality integrated. feat: FooterNav template added.

--- src/QUI/TemplateAvadaAgency/EventHandler.php
+++ src/QUI/TemplateAvadaAgency/EventHandler.php
@@ -41,4 +41,39 @@ class EventHandler
             QUI\System\Log::writeException($Exception);
         }
     }
+
+    /**
+     * Event : on smarty init
+     * @param \Smarty $Smarty - \Smarty
+     */
+    public static function onSmartyInit($Smarty)
+    {
+        // {pace}
+        if (!isset($Smarty->registered_plugins['function']) ||
+            !isset($Smarty->registered_plugins['function']['fetch'])
+        ) {
+            $Smarty->registerPlugin(
+                "function",
+                "fetch",
+                "\\QUI\\TemplateAvadaAgency\\EventHandler::fetch"
+            );
+        }
+    }
+
+    /**
+     * @param $params
+     * @param $Smarty
+     * @return string
+     */
+    public static function fetch($params, $Smarty)
+    {
+        $template = $params['template'];
+        $path     = OPT_DIR . 'quiqqer/template-avadaagency/';
+
+        $Engine = QUI::getTemplateManager()->getEngine();
+        $Engine->assign($params);
+
+        return $Engine->fetch($path . $template);
+    }
+
 }
