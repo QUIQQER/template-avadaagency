e03365c796c640164b9a99805768b100029f7c1a
michael.daniel <michael@pcsg.de>
2018-12-03 08:57:18 +0100

    feat: toTop icon in footer.

--- bin/init.js
+++ bin/init.js
@@ -2,6 +2,14 @@ require(['MooTools'], function () {
     window.addEvent("domready", function () {
         "use strict";
 
+        var ToTop = document.getElements('[href=#top]');
+        if (ToTop) {
+            ToTop.addEvent('click', function (event) {
+                event.stop();
+                new Fx.Scroll(window).toTop();
+            });
+        }
+
         require(['qui/QUI', 'utils/Controls'], function (QUI, Controls) {
             QUI.addEvent("onError", function (msg, url, linenumber) {
                 console.error(msg);
